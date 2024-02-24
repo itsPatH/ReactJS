c5e50e86db8d348544de09bb5d672be696711fe8
Patricia <herrerapatriciadg@gmail.com>
2024-02-24 10:49:03 -0300

    agrego proyecto

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import ItemListContainer from "./components/ItemListContainer";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <ItemListContainer greeting="Bienvenido a la Mishi Store" />
+    </>
+  );
+}
+
+export default App;
